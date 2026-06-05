57db6ef087f96243070a26fd723b01fe6fce7c19
Siurylourenco <siurylourenco@gmail.com>
2026-06-04 22:34:29 -0300

    Adding classes genereted from StarUML

---
+++ code/entity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from  import 
+
+
+class Entity():
+    def __init__(self):
+        self.name = None
+        self.surf = None
+        self.react = None
+
+    def move(self, ):
+        pass
